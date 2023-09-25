0c544e45d5a56b062afc2dcf9fdb9e61abe0fe05
naresh <nareshnaik209080@gmail.com>
2023-09-25 19:55:03 +0530

    JavaScript-Calculator

---
+++ Js/calc.js
@@ -0,0 +1,12 @@
+ 
+//dom
+let answer = document.getElementById("answer");
+let button = document.querySelectorAll('button');
+
+//result
+let ScreenValue = '';
+
+for(let item of button) {
+    item.addEventListener('click',)
+}
+
